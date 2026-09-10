bc1aa46f96cf4f10ee41f7e378ffb0e7086ff778
Rafaell3 <21520240032@maristabrasil.g12.br>
2026-09-10 12:11:36 -0300

    Create exercicio19.js

---
+++ exercicio19.js
@@ -0,0 +1,5 @@
+// Resposta:
+// let é usado quando o valor de uma variável pode mudar durante o programa.
+// const é usado quando o valor não deve ser reatribuído.
+// Exemplo: let contador = 0; pode mudar depois.
+// Exemplo: const PI = 3.14; não deve ser alterado.
